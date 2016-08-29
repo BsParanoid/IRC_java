08310c8c9f46ae8654cb86b0f052863fdb68d662
Ervin <ervin.duttoo@epitech.eu>
2016-08-29 21:37:11 +0200

    Fonction catchMessageToString completely working x*xmsg repush

---
+++ Server/src/MultiClient.java
@@ -0,0 +1,43 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class MultiClient implements Runnable
+{
+    private Socket _socketServer;
+    private int _nbrclient;
+    private ServerSocket _socketServerInstance;
+    
+    Server server = new Server();
+    
+    MultiClient(Socket socketServer, int nbrclient, ServerSocket socketServerInstance)
+    {
+	// TODO Auto-generated constructor stub
+	_socketServer = socketServer;
+	_nbrclient = nbrclient;
+	_socketServerInstance = socketServerInstance;
+    }
+
+    @Override
+    public void run()
+    {
+	// TODO Auto-generated method stub
+	try 
+	{
+    	   while (true)
+    	   {
+    	       _socketServer = _socketServerInstance.accept();
+    	       server.sendMessage("Bonjour client ->"+_nbrclient+" "+_socketServer, _socketServer);
+    	       server.sendMessage("Bonjour client XXXXXXXXXXXXXXXXXXXX"+_nbrclient+" "+_socketServer, _socketServer);
+    	       server.sendMessage("3 eme msg", _socketServer);
+    	       System.out.println("Client nb  "+_nbrclient+ " is connected !");
+    	       _nbrclient++;
+    	       _socketServer.close();
+    	   }
+	}
+	catch (IOException e) 
+	{
+	    e.printStackTrace();
+	}
+    }
+}
---
+++ Server/src/Server.java
@@ -0,0 +1,71 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.io.PrintWriter;
+
+
+public class Server
+{
+    private InetAddress _localAddress;
+    private BufferedReader _in;
+    private PrintWriter _out;
+    private String _recvMessage;
+    private int _numPort = 6667;
+    private int _nbrUser = 3;
+    protected ServerSocket _socketServerInstance;
+    protected Socket _socketServer;
+    protected int _nbrclient = 1;
+    
+    Server(String s)
+    {
+	System.out.println("Récup des adress local et serveur " + s);
+    }
+    public Server()
+    {
+	// TODO Auto-generated constructor stub
+    }
+    void launchServer()
+    {
+	try
+	{
+	    _localAddress = InetAddress.getLocalHost();
+	    _socketServerInstance = new ServerSocket(_numPort, _nbrUser, _localAddress);
+	    Thread threadClient = new Thread(new MultiClient(_socketServer, _nbrclient, _socketServerInstance));
+	    System.out.println("Server is running listening on port ->  "+_socketServerInstance.getLocalPort());
+	    threadClient.start();
+	    System.out.println("A client just connected ");
+	    //sendMessage("You are connected on the server using port -> " +_socketServerInstance.getLocalPort());
+	    while(_socketServer != null);
+	}
+	catch (IOException e) 
+	{
+	    e.printStackTrace();
+	}
+    }
+    void sendMessage(String msg) throws IOException
+    {
+	_out = new PrintWriter(_socketServer.getOutputStream());
+	_out.println(msg);
+	_out.flush();
+    }
+    void sendMessage(String msg,Socket s) throws IOException
+    {
+	_out = new PrintWriter(s.getOutputStream());
+	_out.println(msg);
+	_out.flush();
+    }
+    String catchMessageToString() throws IOException
+    {
+	_in = new BufferedReader(new InputStreamReader(_socketServer.getInputStream()));
+	_recvMessage = _in.readLine();
+	System.out.println(_recvMessage);
+	
+	return _recvMessage;
+    }
+}
