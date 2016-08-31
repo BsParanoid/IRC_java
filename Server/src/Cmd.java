edc61f28418107588ad24f2fc9b41c590aacdc8f
Ervin <ervin.duttoo@epitech.eu>
2016-08-31 05:22:32 +0200

    starting to implement cmd

--- Server/src/Cmd.java
+++ Server/src/Cmd.java
@@ -1,5 +1,44 @@
 
 public interface Cmd
 {
-    void pass(String buffer);
+    public void pass(String buffer);
+    public void nick(String buffer);
+    public void user(String buffer);
+    public void server(String buffer);
+    public void opper(String buffer);
+    public void quit(String buffer);
+    public void squit(String buffer);
+    public void join(String buffer);
+    public void part(String buffer);
+    public void mode(String buffer);
+    public void topic(String buffer);
+    public void names(String buffer);
+    public void list(String buffer);
+    public void invite(String buffer);
+    public void kick(String buffer);
+    public void version(String buffer);
+    public void stats(String buffer);
+    public void links(String buffer);
+    public void time(String buffer);
+    public void connect(String buffer);
+    public void trace(String buffer);
+    public void admin(String buffer);
+    public void info(String buffer);
+    public void privmsg(String buffer);
+    public void notice(String buffer);
+    public void who(String buffer);
+    public void whois(String buffer);
+    public void whowas(String buffer);
+    public void kill(String buffer);
+    public void ping(String buffer);
+    public void pong(String buffer);
+    public void error(String buffer);
+    public void away(String buffer);
+    public void rehash(String buffer);
+    public void restart(String buffer);
+    public void summon(String buffer);
+    public void users(String buffer);
+    public void wallops(String buffer);
+    public void userhost(String buffer);
+    public void ison(String buffer);
 }
--- Server/src/ThreadRead.java
+++ Server/src/ThreadRead.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.PasswordAuthentication;
 import java.net.Socket;
 
 public class ThreadRead implements Runnable
